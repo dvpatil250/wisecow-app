e8eb59b9d1ee91cdd1a4a11e3cf129432b10b544
Dnyaneshwar Gawate <dvpatil0007@gmail.com>
2024-07-26 13:23:08 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -7,40 +7,41 @@ rm -f $RSPFILE
 mkfifo $RSPFILE
 
 get_api() {
-read line
-echo $line
+    read line
+    echo $line
 }
 
 handleRequest() {
-# Process the request
-get_api
-mod=`fortune`
+    # Process the request
+    quote="If you sow your wild oats, hope for a crop failure."
+    mod=`cowsay "$quote"`
 
-cat <<EOF > $RSPFILE
+    cat <<EOF > $RSPFILE
 HTTP/1.1 200 OK
 
-<pre>`cowsay $mod`</pre>
+<pre>$mod</pre>
 EOF
 }
 
 prerequisites() {
-command -v cowsay >/dev/null 2>&1 &&
-command -v fortune >/dev/null 2>&1 ||
-{
-echo "Install prerequisites."
-exit 1
-}
+    command -v cowsay >/dev/null 2>&1 &&
+    command -v fortune >/dev/null 2>&1 ||
+    {
+        echo "Install prerequisites."
+        exit 1
+    }
 }
 
 main() {
-prerequisites
-echo "Wisdom served on port=$SRVPORT..."
+    prerequisites
+    echo "Wisdom served on port=$SRVPORT..."
 
-while [ 1 ]; do
-# Remove the -N option from netcat
-cat $RSPFILE | nc -l $SRVPORT | handleRequest
-sleep 0.01
-done
+    while [ 1 ]; do
+        # Remove the -N option from netcat
+        cat $RSPFILE | nc -l $SRVPORT | handleRequest
+        sleep 0.01
+    done
 }
 
 main
+
